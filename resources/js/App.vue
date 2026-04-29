3b68c31dcb1806e68fa59e7bbc7cf4b8d7fc9060
razyertxy <rayzertzy189@gmail.com>
2026-04-28 18:34:29 -0600

    update

---
+++ resources/js/App.vue
@@ -0,0 +1,7 @@
+
+
+<template>
+  <VApp>
+    <RouterView />
+  </VApp>
+</template>
---
+++ resources/js/pages/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 class="text-h4 font-weight-medium mb-2">
+      Inicio
+    </h1>
+    <p class="text-body-1 text-medium-emphasis mb-6">
+      Resumen de tu contabilidad.
+    </p>
+
+    <VRow>
+      <VCol cols="12">
+        <VCard>
+          <VCardText class="py-8 text-center text-medium-emphasis">
+            Panel principal 
+          </VCardText>
+        </VCard>
+      </VCol>
+    </VRow>
+  </div>
+</template>
