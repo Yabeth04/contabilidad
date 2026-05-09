0e6dd2164e6db3fe37cab9384eef047cbf97ac1b
razyertxy <rayzertzy189@gmail.com>
2026-05-08 21:35:46 -0600

    add axios

--- resources/js/pages/moduleAccounting.vue
+++ resources/js/pages/moduleAccounting.vue
@@ -1,17 +1,5 @@
 <script setup>
-import { useRoute } from 'vue-router'
 
-const route = useRoute()
-const activeTab = ref(route.params.tab)
-
-// tabs
-const tabs = [
-  {
-    title: 'Account',
-    icon: 'ri-group-line',
-    tab: 'account',
-  },
-]
 </script>
 
 <template>
